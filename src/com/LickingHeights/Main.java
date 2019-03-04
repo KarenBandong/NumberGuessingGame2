9425a13677809140306482e5d2432dc12dccc5db
Karen <karen.bandong.2018@student.lhschools.org>
2019-03-04 07:44:21 -0500

    Phase 1 and 2 created

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,23 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Phase 1 and 2
+        int numberNeeded;
+        String Username;
+        Scanner keyboard= new Scanner(System.in);
+
+
+
+
+
+        System.out.println("Hello! Welcome to the Number Guessing Game. May I please have your name?");
+        Username= keyboard.nextLine();
+        System.out.println("Hi"+Username+"We may now start the game...");
+        System.out.println("Pick a number from 1-100");
+        
     }
 }
